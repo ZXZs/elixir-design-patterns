ea65d742fcc34df6022229bfb97ce13b520c6707
Vladislav <double-vh@yandex.ru>
2019-07-05 17:39:11 +0600

    fix mediator

--- behavioral/mediator/README.md
+++ behavioral/mediator/README.md
@@ -4,30 +4,21 @@
 $ iex -pr run.exs
 Interactive Elixir (1.9.0-rc.0) - press Ctrl+C to exit (type h() ENTER for help)
 
-iex(1)> {:ok, chat} = TextChat.init(nil, [])
-{:ok, #PID<0.118.0>}
+# initializing
 
-iex(2)> {:ok, admin} = Admin.init(chat)
-{:ok, #PID<0.120.0>}
+iex(1)> {:ok, mediator} = Mediator.init()
+{:ok, #PID<0.109.0>}
 
-iex(3)> {:ok, u1} = User.init(chat)
-{:ok, #PID<0.122.0>}
+iex(2)> {:ok, user1} = User.init(mediator)
+{:ok, #PID<0.111.0>}
 
-iex(4)> {:ok, u2} = User.init(chat)
-{:ok, #PID<0.124.0>}
+iex(3)> {:ok, user2} = User.init(mediator)
+{:ok, #PID<0.113.0>}
 
-iex(5)> send chat, {:set, admin}
-{:set, #PID<0.120.0>}
+# let's try to send message
 
-iex(6)> send chat, {:add, u1}
-{:add, #PID<0.122.0>}
+iex(4)> send user1, {:send, [message: "Hello, drugan!", to: user2]}
+{:send, [message: "Hello, drugan!", to: #PID<0.113.0>]}
+{"Got message:", "Hello, drugan!", "from:", #PID<0.111.0>}
 
-iex(7)> send chat, {:add, u2}
-{:add, #PID<0.124.0>}
-
-iex(8)> send u1, {:send, "Hello, i am user!"}
-{:send, "Hello, i am user!"}
-
-iex(9)> send admin, {:send, "ADMIN is here!!!"}
-{:send, "ADMIN is here!!!"}
 ```
--- behavioral/mediator/run.exs
+++ behavioral/mediator/run.exs
@@ -1,76 +1,35 @@
-defmodule Admin do
-	defp loop(chat) do
-		receive do
-			{:send, message} ->
-				send chat, {:msg, [user: self(), message: message]}
-				loop(chat)
-			{:get, message} ->
-				"Admin got message \"" <> message <> "\""
-				loop(chat)
-		end
-	end
-
-	def init(chat) do
-		Task.start_link fn -> loop(chat) end
-	end
-end
-
 defmodule User do
-	defp loop(chat) do
+	defp loop(mediator) do
 		receive do
-			{:send, message} ->
-				send chat, {:msg, [user: self(), message: message]}
-				loop(chat)
+			{:send, [message: message, to: to]} ->
+				send mediator , {:send, [
+					message: message,
+					from: self(),
+					to: to
+				]}
+				loop(mediator)
 
-			{:get, message} ->
-				"User got message \"" <> message <> "\""
-				loop(chat)
+			{:receive, [message: message, from: from]} ->
+				IO.inspect {"Got message:", message, "from:", from}
+				loop(mediator)
 		end
 	end
 
-	def init(chat) do
-		Task.start_link fn -> loop(chat) end
+	def init(mediator) do
+		Task.start_link(fn -> loop(mediator) end)
 	end
 end
 
-defmodule TextChat do
-	defp loop(admin, users) do
+defmodule Mediator do
+	defp loop() do
 		receive do
-			{:set, admin} ->
-				loop(admin, users)
-
-			{:add, user} ->
-				loop(admin, users ++ [user])
-
-			{:send, [user: user, message: message]} ->
-				users |> Enum.each(fn x -> send x, {:get, message} end)
-				send admin, {:get, message}
+			{:send, [message: message, from: from, to: to]} ->
+				send to , {:receive, [message: message, from: from]}
+				loop()
 		end
 	end
 
-	def init(admin, users) do
-		Task.start_link fn -> loop(admin, users) end
+	def init() do
+		Task.start_link(fn -> loop() end)
 	end
 end
-
-defmodule App do
-	def main do
-		{:ok, chat} = TextChat.init(nil, [])
-
-		{:ok, admin} = Admin.init(chat)
-
-		{:ok, u1} = User.init(chat)
-		{:ok, u2} = User.init(chat)
-
-		send chat, {:set, admin}
-
-		send chat, {:add, u1}
-		send chat, {:add, u2}
-
-		send u1, {:send, "Hello, i am user"}
-
-		send admin, {:send, "ADMIN is here!!!"}
-	end
-end
-
-App.main
