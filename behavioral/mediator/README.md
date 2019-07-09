3c8ab36a4034ca5b59f5524db94bb175f314d8e1
Vladislav <double-vh@yandex.ru>
2019-07-09 20:38:25 +0600

    reimplement "mediator" pattern with GenServer

--- behavioral/mediator/README.md
+++ behavioral/mediator/README.md
@@ -4,21 +4,15 @@
 $ iex -pr run.exs
 Interactive Elixir (1.9.0-rc.0) - press Ctrl+C to exit (type h() ENTER for help)
 
-# initializing
-
-iex(1)> {:ok, mediator} = Mediator.init()
+iex(1)> {:ok, mediator} = Mediator.start_link
 {:ok, #PID<0.109.0>}
 
-iex(2)> {:ok, user1} = User.init(mediator)
+iex(2)> {:ok, user1} = User.start_link(mediator)
 {:ok, #PID<0.111.0>}
 
-iex(3)> {:ok, user2} = User.init(mediator)
+iex(3)> {:ok, user2} = User.start_link(mediator)
 {:ok, #PID<0.113.0>}
 
-# let's try to send message
-
-iex(4)> send user1, {:send, [message: "Hello, drugan!", to: user2]}
-{:send, [message: "Hello, drugan!", to: #PID<0.113.0>]}
-{"Got message:", "Hello, drugan!", "from:", #PID<0.111.0>}
-
+iex(4)> user1 |> User.send(["Hello, user2!", to: user2])
+:ok
 ```
--- behavioral/mediator/run.exs
+++ behavioral/mediator/run.exs
@@ -1,35 +1,54 @@
 defmodule User do
-	defp loop(mediator) do
-		receive do
-			{:send, [message: message, to: to]} ->
-				send mediator , {:send, [
-					message: message,
-					from: self(),
-					to: to
-				]}
-				loop(mediator)
-
-			{:receive, [message: message, from: from]} ->
-				IO.inspect {"Got message:", message, "from:", from}
-				loop(mediator)
-		end
-	end
+	use GenServer
 
 	def init(mediator) do
-		Task.start_link(fn -> loop(mediator) end)
+		{:ok, mediator}
+	end
+
+	def handle_cast({:send, [message, to: to]}, mediator) do
+		mediator |> Mediator.send([message, from: self(), to: to])
+		{:noreply, mediator}
+	end
+
+	def handle_cast({:receive, [message, from: from]}, mediator) do
+		IO.inspect [message, from: from]
+		{:noreply, mediator}
+	end
+
+	###########################################################################
+
+	def start_link(mediator) do
+		GenServer.start_link(__MODULE__, :ok, [mediator])
+	end
+
+	def send(user, [message, to: to]) do
+		GenServer.cast(user, {:send, [message, to: to]})
+	end
+
+	def receive(user, [message, from: from]) do
+		GenServer.cast(user, {:receive, [message, from: from]})
 	end
 end
 
 defmodule Mediator do
-	defp loop() do
-		receive do
-			{:send, [message: message, from: from, to: to]} ->
-				send to , {:receive, [message: message, from: from]}
-				loop()
-		end
+	use GenServer
+
+	def init(:ok) do
+		{:ok, true}
 	end
 
-	def init() do
-		Task.start_link(fn -> loop() end)
+	def handle_cast({:send, [message, from: from, to: to]}) do
+		to |> User.receive([message, from: from])
+		:noreply
+	end
+
+	###########################################################################
+
+	def start_link do
+		GenServer.start_link(__MODULE__, :ok, [])
+	end
+
+	def send(mediator, [message, from: from, to: to]) do
+		GenServer.cast mediator, {:send, [message, from: from, to: to]}
 	end
 end
